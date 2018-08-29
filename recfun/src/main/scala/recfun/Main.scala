8853fe712042c1dcf5f99460235b08a4ee51e6dc
joprietoe <joprietoe@gmail.com>
2018-08-29 16:54:49 -0300

    OK

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -46,5 +46,10 @@ object Main {
    */
     def countChange(money: Int, coins: List[Int]): Int = {
       
+      if(coins.isEmpty || money < 0) 0
+      else if(money == 0 && !coins.isEmpty) 1
+      else  countChange(money - coins.head, coins) +
+            countChange(money, coins.tail)
+
     }
   }
